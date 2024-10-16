a28df55135aadfb70c7a49643fa836b8a079d784
maria <150175935+mmmaryae@users.noreply.github.com>
2024-10-16 16:43:38 -0300

    array

--- 04-arrays.php
+++ 04-arrays.php
@@ -28,7 +28,44 @@
 
         <h2>Acessando os dados do array</h2>
         <p>Rock Progressivo: <?= $bandas[2] ?> </p>
-        <p>Ontem stivemos com a tuma de <?= $cursos[2] ?> </p>
+        <p>Ontem estivemos com a tuma de <?= $cursos[2] ?>. </p>
+
+        <hr>
+
+
+        <?php
+
+        // Array associativo
+        $filme = [
+            // '=>' associação
+            "titulo" => "Senhor dos Anéis",
+            "autor" => "J.R.R Tolkien",
+            "lancamento" => 2001,
+            "genero" => "Fantasia"
+
+        ];
+
+        ?>
+
+        <h2>Acessando os dados do array associativo</h2>
+
+        <p>O filme <?= $filme["titulo"] ?> foi lançado em
+            <?= $filme["lancamento"] ?>.</p>
+
+        <hr>
+
+        <h2>Comandos úteis(para programadores) para análise de arrays</h2>
+
+        <!--  <pre> muda a letra e faz uma indentação  organiza-->
+
+        <h3>print_r(nomeArray)</h3>
+        <pre> <?= print_r($bandas) ?> </pre>
+
+        <h3>var_dump(nomeArray)</h3>
+        <!-- pega o estilo e tamanho -->
+        <pre> <?= var_dump($bandas) ?> </pre>
+
+
     </div>
 
 
